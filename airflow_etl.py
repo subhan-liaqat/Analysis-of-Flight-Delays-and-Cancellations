eeedab8f433960f7771819634c5c7825f82de17a
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2023-12-13 02:05:36 +0500

    Update airflow_etl.py

--- airflow_etl.py
+++ airflow_etl.py
@@ -1,77 +1,45 @@
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.providers.snowflake.transfers.postgres_to_snowflake import PostgresToSnowflakeOperator
 from datetime import datetime, timedelta
-from airflow.decorators import dag, task
-from airflow.operators.python import PythonOperator
-from airflow.contrib.operators.snowflake_operator import SnowflakeOperator
-from airflow.contrib.hooks.snowflake_hook import SnowflakeHook
-from sqlalchemy import create_engine
-import requests
-import pandas as pd
 
+
+# Define default_args and schedule_interval
 default_args = {
-    "owner": "Subhan",
-    "retries": 2,
-    "retry_delay": timedelta(minutes=5)
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 12, 10),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
-@dag (
-    dag_id = "flights_etl",
-    start_date = datetime(2023, 12, 10),
-    default_args = default_args,
-    schedule = "@daily",
-    catchup = False
+
+dag = DAG(
+    'postgres_to_snowflake',
+    default_args=default_args,
+    schedule_interval='@daily',
 )
 
-def pipeline():
-
-    @task
-    def get_flights_data(start_date = datetime.now()):
-    
-        flights_data = pd.read_csv(csv_files['flights'])
-        
-        # getting the data into a dataframe and dropping unnecessary info, converting the time column type to datetime.
-       flights_data = flights_data.drop(["YEAR", "WEATHER_DELAY", "WEATHER_DELAY", "AIRLINE_DELAY", "SECURITY_DELAY", "AIR_SYSTEM_DELAY"], axis=1)       
-       return flights_data;
-    
-    # Checking if the DF is empty or contains Null values and raising an exception if either is true.
-    @task
-    def check_data(flights_data):
-
-        if flights_data.empty:
-            raise Exception("Dataframe is empty")
-        elif flights_data.isna().sum().sum() > 0:
-            raise Exception("Dataframe contains Null values")
-
-    # Pandas dataframe to Snowflake  
-    @task(trigger_rule = "all_success")
-    def df_to_snowflake(flights_data):
-        
-        snowflake_hook = SnowflakeHook(snowflake_conn_id = "subhanliaqat")
-        conn = snowflake_hook.get_uri()
-        engine = create_engine(conn)
-        flights_data.to_sql("flights", con = engine, index = False, if_exists = "append")
-
-    # Creates the table to snowflake, using the connection that has been defined in Airflow Web UI
-    task0 = SnowflakeOperator(
-        task_id = "create_table",
-        sql = "CREATE TABLE flights",
-        snowflake_conn_id = "subhanliaqat"
-    )
-
-    task1 = get_flights_data()
-
-    task2 = check_data(task1)
-
-    task3 = SnowflakeOperator(
-        task_id = "clear_table",
-        trigger_rule = "all_success",
-        sql = "DELETE FROM flights_data",
-        snowflake_conn_id = "subhanliaqat"
-    )
-
-    task4 = df_to_snowflake(task1)
+extract_postgres_task = PostgresOperator(
+    task_id='extract_postgres_data',
+    sql='sql/extract_postgres_data.sql',
+    postgres_conn_id='postgres123',
+    dag=dag,
+)
 
 
+load_snowflake_task = PostgresToSnowflakeOperator(
+    task_id='load_snowflake_data',
+    sql='sql/load_snowflake_data.sql',
+    postgres_conn_id='postgres123',
+    snowflake_conn_id='subhanliaqat',
+    schema='flights',
+    table='flights',
+    dag=dag,
+)
 
-    task0 >> task1 >> task2 >> task3 >> task4
 
-pipeline()
+# Define task dependencies
+extract_postgres_task >> load_snowflake_task
